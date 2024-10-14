536f4ae66f704e97764ffb497091578e1525deb0
Ben Moeller <benlynnmoeller@gmail.com>
2024-10-13 23:50:27 -0500

    adding new parameter to manifest and putting base in function

--- contextSentenceHider.js
+++ contextSentenceHider.js
@@ -1,18 +1,21 @@
 const blurStyles = "color: transparent; text-shadow: 0 0 5px rgba(0,0,0,0.5);";
+findContextSentences();
 
-const allH3Elements = document.querySelectorAll('h3');
-for (const h3Element of allH3Elements) {
-    const innerText = h3Element.innerText.toLowerCase();
-    if (innerText !== 'context sentences') {
-        continue
-    }
+function findContextSentences() {
+    const allH3Elements = document.querySelectorAll('h3');
+    for (const h3Element of allH3Elements) {
+        const innerText = h3Element.innerText.toLowerCase();
+        if (innerText !== 'context sentences') {
+            continue
+        }
 
-    const parent = h3Element.parentElement;
-    if (!parent) {
-        continue;
+        const parent = h3Element.parentElement;
+        if (!parent) {
+            continue;
+        }
+        addBlurLogicToContextSentences(parent);
     }
-    addBlurLogicToContextSentences(parent);
-}
+};
 
 function addBlurLogicToContextSentences(contextSentencesParent) {
     const contextSentenceChildren = contextSentencesParent.children;
