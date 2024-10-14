894f3bef69f012413785c7b9478d80821e4108a3
Ben Moeller <benlynnmoeller@gmail.com>
2024-10-13 23:31:33 -0500

    Updating logic to be more generic

--- contextSentenceHider.js
+++ contextSentenceHider.js
@@ -1,8 +1,39 @@
-const contextSentenceElements = document.querySelectorAll('.context-sentences');
+const blurStyles = "color: transparent; text-shadow: 0 0 5px rgba(0,0,0,0.5);";
 
-for (const contextSentenceElement of contextSentenceElements) {
-    const child = contextSentenceElement.children[1];
-    if (child) {
-        child.innerHTML = 'hi';
+const allH3Elements = document.querySelectorAll('h3');
+for (const h3Element of allH3Elements) {
+    const innerText = h3Element.innerText.toLowerCase();
+    if (innerText !== 'context sentences') {
+        continue
     }
+
+    const parent = h3Element.parentElement;
+    if (!parent) {
+        continue;
+    }
+    addBlurLogicToContextSentences(parent);
 }
+
+function addBlurLogicToContextSentences(contextSentencesParent) {
+    const contextSentenceChildren = contextSentencesParent.children;
+    for (const contextSentenceDiv of contextSentenceChildren) {
+        const englishText = contextSentenceDiv.children[1];
+        if (!englishText) {
+            continue;
+        }
+    
+        englishText.setAttribute('style', blurStyles);
+        addOnClickFunctionalityToEnglishText(englishText);
+    }
+};
+
+function addOnClickFunctionalityToEnglishText(englishText) {
+    englishText.addEventListener('click', () => {
+        const currentStyle = englishText.getAttribute('style');
+        if (!currentStyle) {
+            englishText.setAttribute('style', blurStyles);
+        } else {
+            englishText.removeAttribute('style');
+        }
+    })
+};
