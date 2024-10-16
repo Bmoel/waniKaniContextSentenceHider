1d536cdaab81de85a00f6f0552163b02bcdbaea3
Ben Moeller <benlynnmoeller@gmail.com>
2024-10-15 23:45:46 -0500

    Limit to only being for elements with class name, blurring better, and formatting

--- contextSentenceHider.js
+++ contextSentenceHider.js
@@ -1,4 +1,4 @@
-const blurStyles = "color: transparent; text-shadow: 0 0 5px rgba(0,0,0,0.5);";
+const blurStyle = "filter: blur(4px);";
 
 var observer = new MutationObserver(() => {
     findContextSentences();
@@ -6,20 +6,6 @@ var observer = new MutationObserver(() => {
 observer.observe(document, { childList: true, subtree: true });
 
 function findContextSentences() {
-    // Find elements for Vocabulary pages
-    const allH3Elements = document.querySelectorAll('h3');
-    for (const h3Element of allH3Elements) {
-        const innerText = h3Element.innerText.toLowerCase();
-        if (innerText !== 'context sentences' || !h3Element.parentElement) {
-            continue;
-        }
-        const contextSentenceChildren = h3Element.parentElement.children;
-        for (const contextSentenceDiv of contextSentenceChildren) {
-            manipulateEnglishText(contextSentenceDiv);
-        }
-    }
-
-    // Find elements for Today's Lessons
     const contextSentenceElements = document.getElementsByClassName('context-sentences');
     for (const parentDiv of contextSentenceElements) {
         manipulateEnglishText(parentDiv);
@@ -32,7 +18,7 @@ function manipulateEnglishText(parentDiv) {
     if (!englishTextElement || englishTextElement.classList.contains(hiderClassName)) {
         return;
     }
-    englishTextElement.setAttribute('style', blurStyles);
+    englishTextElement.setAttribute('style', blurStyle);
     addOnClickFunctionalityToEnglishText(englishTextElement);
     englishTextElement.classList.add(hiderClassName);
 };
@@ -42,7 +28,7 @@ function addOnClickFunctionalityToEnglishText(englishText) {
         const englishTextElement = event.target;
         const currentStyle = englishTextElement.getAttribute('style');
         if (!currentStyle) {
-            englishTextElement.setAttribute('style', blurStyles);
+            englishTextElement.setAttribute('style', blurStyle);
         } else {
             englishTextElement.removeAttribute('style');
         }
